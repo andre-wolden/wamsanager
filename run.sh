a012d401a73d5c0cef79487697ac77a5f4f8be6f
André Wolden <wolden.andre@gmail.com>
2018-04-29 12:22:37 +0200

    kajsfnakjnf

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 # author: Andre Wolden
 
 
-export SPRING_DATASOURCE_URL="jdbc:postgresql://localhost/wamsanagerbase"
-export SPRING_DATASOURCE_USERNAME="localuser"
-export SPRING_DATASOURCE_PASSWORD="password"
+# export SPRING_DATASOURCE_URL="jdbc:postgresql://localhost/wamsanagerbase"
+#export SPRING_DATASOURCE_USERNAME="localuser"
+#export SPRING_DATASOURCE_PASSWORD="password"
 
 mvn clean install
 java -jar target/wamsanager-0.0.1-SNAPSHOT.jar
